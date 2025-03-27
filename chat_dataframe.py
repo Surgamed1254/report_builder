c1d86f3742dacb65620ebfb91f626347075c640c
Surgamed1254 <mweinreb@surgamed.com>
2025-03-27 03:55:39 +0300

    Update chat_dataframe.py

--- chat_dataframe.py
+++ chat_dataframe.py
@@ -107,10 +107,10 @@ def search_dataframe(user_input):
             report_dict[ref_id] = {"item_title": row["Item Title"], "customer_list": []}
         report_dict[ref_id]["customer_list"].append({
             "name": row["Name"],
-            "phone": row["customer_phone"],
+            "phone": row["customer_phone"]??"",
             "email": row["customer_email"],
-            "quantity": row["Quantity"],
-            "price": row["price"],
+            "quantity": str(row["Quantity"]),
+            "price": str(row["price"]),
             'date':row["Date"]
         })
     
